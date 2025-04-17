b1ab64e4285ff97c533e8f6c659b9ca4dc4d6930
Gustav Schneider <gustav.nschneider@gmail.com>
2025-04-17 22:41:41 +0200

    Small windows related rework of cmake file

--- src/ImGui_init.cpp
+++ src/ImGui_init.cpp
@@ -10,8 +10,9 @@ void error_callback(int error, const char* description) {
 }
 
 void debugCallbackFun(GLenum source, GLenum type, GLuint id, GLenum severity,
-                      GLsizei length,const GLchar *message, const void *userParam) {
-  puts(message);
+                      GLsizei length,const GLchar *message, const void *userParam)
+{
+    //puts(message);
 }
 
 void
